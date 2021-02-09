6be5f56c8be88eec056e8d30d27d276dc67c44c2
appcreatorguy <manmenonsense@gmail.com>
2021-02-09 16:25:13 +0000

    # Added Entities, with respective Classes Also added jobs, and Player

---
+++ Jobs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bordertale
+{
+    public class Job
+    {
+        public int max;
+        public int hp;
+        public int ap;
+        public int heal;
+        // TODO Add Weapons Class
+        public Job(int max, int ap, bool canHeal = false, int heal = 0)
+        {
+            this.max = max;
+            this.ap = ap;
+            if (canHeal)
+            {
+                this.heal = heal;
+            }
+        }
+    }
+    public class Jobs
+    {
+        public Job fighter = new Job(120, 40);
+        public Job wizard = new Job(300, 20);
+        public Job healer = new Job(200, 20, true, 40);
+    }
+}
