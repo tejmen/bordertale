febb25bf6f2d0cf288612170b9b379907525042b
Meena Mengle <tej_men@outlook.com>
2021-02-09 08:36:06 +0000

    Added Help menu

--- MainGame.cs
+++ MainGame.cs
@@ -13,7 +13,7 @@ namespace bordertale
             Console.WriteLine("|       ||  | |  ||   |_||_ | | |   ||   |___ |   |_||_   |   |  |       ||   |    |   |___ ");
             Console.WriteLine("|  _   | |  |_|  ||    __  || |_|   ||    ___||    __  |  |   |  |       ||   |___ |    ___|");
             Console.WriteLine("| |_|   ||       ||   |  | ||       ||   |___ |   |  | |  |   |  |   _   ||       ||   |___ ");
-            Console.WriteLine("|_______||_______||___|  |_||______| |_______||___|  |_|  |___|  |__| |__||_______||_______|");
+            Console.WriteLine("|_______||_______||___|  |_||______| |_______||___|  |_|  |___|  |__| |__||_______||_______|\n");
             PrintUtils.GetHash(width);
             PrintUtils.CenterPadHash("Welcome to Bordertale!", width);
             PrintUtils.CenterPadHash("- Play -", width);
@@ -23,13 +23,30 @@ namespace bordertale
             PrintUtils.CenterPadHash("- Quit -", width);
             PrintUtils.CenterPadHash("Copyright 2021 tejmen09", width);
             PrintUtils.GetHash(width);
+            
         }
-        public static void HelpScreen(bool inGame)
+        public static void HelpScreen()
         {
             int width = 31;
             PrintUtils.GetHash(width);
             PrintUtils.CenterPadHash("Help", width);
-            PrintUtils.LeftPadHash("", width);
+            PrintUtils.LeftPadHash("• Type 'move' command to", width);
+            PrintUtils.LeftPadHash(" move", width);
+            PrintUtils.LeftPadHash("• Type your commands to do", width);
+            PrintUtils.LeftPadHash(" them", width);
+            PrintUtils.LeftPadHash("• Type 'look' to inspect", width);
+            PrintUtils.LeftPadHash(" something", width);
+            PrintUtils.LeftPadHash("• Type 'act' to do what you", width);
+            PrintUtils.LeftPadHash("can on your place", width);
+            PrintUtils.LeftPadHash("• If you find a Dungeon,", width);
+            PrintUtils.LeftPadHash(" please help to excavate it", width);
+            PrintUtils.LeftPadHash("• Find more weapons hidden", width);
+            PrintUtils.LeftPadHash(" in chests to kill monsters", width);
+            PrintUtils.LeftPadHash("• Go to the store to buy", width);
+            PrintUtils.LeftPadHash(" armour", width);
+            PrintUtils.LeftPadHash("• Equip your armour for", width);
+            PrintUtils.LeftPadHash(" extra protection.", width);
+            PrintUtils.CenterPadHash("Copyright 2019 tejmen09", width);
         }
     }
 }
