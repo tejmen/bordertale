6fd69181fad1b954de39b95370722bea5b4d9376
appcreatorguy <manmenonsense@gmail.com>
2021-03-04 16:32:14 +0000

    Added PrintUtilsTests for GetHash() and CenterPadHash()

--- bordertaleTests/PrintUtilsTests.cs
+++ bordertaleTests/PrintUtilsTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace bordertale.Tests
 {
@@ -12,9 +13,37 @@ namespace bordertale.Tests
     public class PrintUtilsTests
     {
         [Test]
-        public void CenterPadHashTest()
+        public void CenterPadHashTest_WithEvenSpacing()
         {
-            Assert.That(1 == 1);
+            // Arrange
+            var input = "Tests";
+            var expected = "#  Tests  #";
+            var expectedConsole = String.Format("{0}{1}", "#  Tests  #", Environment.NewLine);
+            var length = 11;
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            // Act
+            PrintUtils.CenterPadHash(input, length);
+            var output = PrintUtils.CenterPadHashReturn(input, length);
+
+            // Assert
+            Assert.That(consoleOutput.ToString(), Is.EqualTo(expectedConsole));
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        [TestCase(14)]
+        [TestCase(2)]
+        [TestCase(214)]
+        public void GetHashTest(int length)
+        {
+            // Arrange
+            var expected = String.Format("{0}{1}", new String('#', length), Environment.NewLine);
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            // Act
+            PrintUtils.GetHash(length);
         }
     }
 }
