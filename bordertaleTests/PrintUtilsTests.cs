8d848c64a4d5d2ec8b5a91f5108c997ab23ad5d0
appcreatorguy <manmenonsense@gmail.com>
2021-03-04 14:23:31 +0000

    Created PrintUitls Tests

---
+++ bordertaleTests/PrintUtilsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bordertale;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bordertale.Tests
+{
+    [TestClass()]
+    public class PrintUtilsTests
+    {
+        [TestMethod()]
+        public void CenterPadHashTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
