4747cd519178f2196885fc5564ed0fd8542b4ded
appcreatorguy <manmenonsense@gmail.com>
2021-04-18 10:15:49 +0100

    created test. should throw exception

---
+++ bordertaleTests/Entities/PlayerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using bordertale.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bordertale.Entities.Tests
+{
+    [TestFixture()]
+    public class PlayerTests
+    {
+        [Test()]
+        public void EquipTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
